81f37de6be26d9fa821ab61bf511d110b61709a1
Zhenwei Wang <zhenwei.wang.1990@gmail.com>
2023-03-10 09:42:08 +0800

    feat: add idn-decode

---
+++ idn-decode/src/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <h-layout-single mode="middle" :width="1280">
+    <h-multiline-input
+          v-model="input"
+          :title="$t('idnEncoding.input')"
+          @change="decode"
+        />
+        <h-multiline-result  :result="output"
+          :result-desc="$t('idnEncoding.output')"
+        />
+  </h-layout-single>
+</template>
+
+<script setup lang="ts">
+import {ref,onMounted} from "vue"
+import {likeIDN} from './util.ts'
+
+import {toUnicode } from "punycode"
+
+const EXAMPLE_IDN_DOMAIN = 'xn--ehqy74e.com'
+const input = ref(EXAMPLE_IDN_DOMAIN)
+const output = ref('')
+const props = defineProps<{
+  clipboardValue?: any
+}>()
+
+const emit = defineEmits(['acceptClipboardCb'])
+onMounted(async () => {
+  $he3.getLastClipboard().then(res => {
+    if (likeIDN(res)) {
+      input.value = res
+      decode()
+      $he3.useClipboardCallBack()
+    }
+  })
+})
+function decode() {
+  output.value = toUnicode(input.value)
+}
+</script>
+
+<style scoped lang="less"></style>
