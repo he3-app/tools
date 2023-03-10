5a2f7333b721738c713a7c023c06cf75e7273f75
Zhenwei Wang <zhenwei.wang.1990@gmail.com>
2023-03-10 10:25:08 +0800

    feat: migrate utf7-encode

---
+++ utf7-encode/src/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <h-layout-single mode="middle" :width="1280">
+    <div>
+    <h-multiline-input
+        select-all-on-mount
+        v-model="input"
+        :title="$t('input')"
+        @change="handleChange"
+      />
+      <h-multiline-result
+        :result="output"
+        :result-desc="$t('output')"
+      />
+    </div>
+  </h-layout-single>
+</template>
+
+<script setup lang="ts">
+import {ref,onMounted} from "vue"
+import {utf7Encode} from "./utf7.js"
+const EXAMPLE_STR = `A Free, Modern Toolbox
+Built for Developers.
+`
+const input = ref(EXAMPLE_STR)
+const output = ref('')
+
+onMounted(() => {
+  handleChange()
+})
+
+function handleChange() {
+  output.value = utf7Encode(input.value)
+}
+</script>
+
+<style scoped lang="less"></style>
