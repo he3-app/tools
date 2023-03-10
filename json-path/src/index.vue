7e17f8a61ca4beeb2a508a53cbcbad4696ead421
zhangze <1183460943@qq.com>
2023-03-10 17:59:38 +0800

    feat: add json path tool

--- json-path/src/index.vue
+++ json-path/src/index.vue
@@ -1,24 +1,117 @@
 <template>
-  <h-layout-single mode="middle" :width="1280">
-      {{t("test")}}
-  </h-layout-single>
+  <h-layout-horizontal mode="middle">
+    <template #left class="left">
+      <h-multiline-input
+          v-model="input"
+          autofocus
+          class="input"
+          :title="t('input')"
+          :code="true"
+          @change="evaluate"
+      />
+    </template>
+    <template #right>
+      <div>
+        <p class="option">
+          <span>{{ t('path') }}</span>
+          <a-input v-model:value="path" class="opt" @change="evaluate"/>
+        </p>
+        <h-multiline-result :result="output" :result-desc="t('output')"/>
+      </div>
+    </template>
+  </h-layout-horizontal>
 </template>
-
-<script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
+<script setup>
+import jp from 'jsonpath'
 import messages from './lang.json'
-const $he3 = window.$he3
-const { t } = useI18n({
+import { useI18n } from 'vue-i18n'
+const {t} = useI18n({
   locale: window.$he3.lang,
   messages
 })
+import {onMounted, ref} from 'vue';
+
+const input = ref(`{
+  "store": {
+    "book": [
+      {
+        "category": "reference",
+        "author": "Nigel Rees",
+        "title": "Sayings of the Century",
+        "price": 8.95
+      }, {
+        "category": "fiction",
+        "author": "Evelyn Waugh",
+        "title": "Sword of Honour",
+        "price": 12.99
+      }, {
+        "category": "fiction",
+        "author": "Herman Melville",
+        "title": "Moby Dick",
+        "isbn": "0-553-21311-3",
+        "price": 8.99
+      }, {
+         "category": "fiction",
+        "author": "J. R. R. Tolkien",
+        "title": "The Lord of the Rings",
+        "isbn": "0-395-19395-8",
+        "price": 22.99
+      }
+    ],
+    "bicycle": {
+      "color": "red",
+      "price": 19.95
+    }
+  }
+}`)
+const output = ref('')
+const path = ref('$..author')
+
+function isJson(text) {
+  if (text.length < 30) {
+    return false
+  }
+
+  if (!text.trim().startsWith('{') && !text.trim().startsWith('\\{')) {
+    return false
+  }
+
+  try {
+    JSON.parse(text)
+  } catch (e) {
+    return false
+  }
+  return true
+}
 
 onMounted(() => {
-  console.log(t('test'))
+  $he3.getLastClipboard().then(res => {
+    if (isJson(res)) {
+      inputValue.value = res
+      $he3.onUseClipboardValue()
+    }
+    evaluate()
+  })
 })
 
-</script>
-<style scoped lang="less">
+const evaluate = () => {
+  if (input.value.length <= 0) {
+    window.$he3.message.warn(t("inputJson"))
+    return
+  }
 
+  if (path.value.length <= 0) {
+    window.$he3.message.warn(t("inputJsonPath"))
+    return
+  }
+  output.value = jp.query(JSON.parse(input.value), path.value).join('\n')
+}
+
+</script>
+
+<style scoped lang="less">
+.left {
+  height: 100%;
+  overflow-y: scroll;
+}
 </style>
