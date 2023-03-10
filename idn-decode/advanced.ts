6c3f85a0f8931a64e3d20bcb25d4fa3a9b71b9a2
Zhenwei Wang <zhenwei.wang.1990@gmail.com>
2023-03-10 10:36:04 +0800

    fix: fix idn decode recommend

--- idn-decode/advanced.ts
+++ idn-decode/advanced.ts
@@ -1,8 +1,9 @@
-import { likeIDN } from "./util";
+import { likeIDN } from "./src/util";
 // 推荐的hook，传入的内容是当前的剪贴板内容，返回 1 ，2， 3， 4， 5， 数字越大，优先级越高
 // 如果你的函数判断的很准，就return 大一点，反之就小一点
 // 输入是剪贴板的值
 function recommandHook(str) {
+  console.log("likeIDN:" + likeIDN(str));
   if (likeIDN(str)) {
     return 5;
   } else {
